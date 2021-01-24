7e66c593690c30df586ff6466af8c59c806cf97c
ido_yeh <ido.yehezkel121992@gmail.com>
2021-01-24 17:45:54 +0200

    new Web

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/', async (req, res) => {
-  // res.sendFile(path.resolve(__dirname + '/../../client/views/home.html'));
-  res.sendFile(path.resolve(__dirname + '/../../client/views/Home_Page.html'));
+  res.sendFile(path.resolve(__dirname + '/../../client/views/home.html'));
+  // res.sendFile(path.resolve(__dirname + '/../../client/views/Home_Page.html'));
   
   // res.send('Hello World!!!');
   // console.log('Hello World!!!');
